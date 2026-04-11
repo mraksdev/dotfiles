524ff909101ad87a01d2b8aee5f8fdd4883b1303
mraksdev <a.krylov.work@gmail.com>
2026-04-11 15:51:33 +0300

    add: tmux config

--- setup.sh
+++ setup.sh
@@ -2,7 +2,9 @@ rm -rf ~/.config/zsh
 rm -f ~/.zshrc
 rm -f ~/.zshenv
 rm -rf ~/.config/nvim
+rm -rf ~/.config/tmux
 
 ln -s ~/dotfiles/zsh/.zshenv ~/.zshenv
 ln -s ~/dotfiles/zsh ~/.config/zsh
 ln -s ~/dotfiles/nvim ~/.config/nvim
+ln -s ~/dotfiles/tmux ~/.config/tmux
