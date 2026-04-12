635de608d120fb661abb22dc6beff1fcd1cd57df
mraksdev <a.krylov.work@gmail.com>
2026-04-12 17:05:13 +0300

    refactor

--- nvim/lua/mraks/plugins/lint.lua
+++ nvim/lua/mraks/plugins/lint.lua
@@ -1,7 +1,3 @@
--- Linting
-
----@module 'lazy'
----@type LazySpec
 return {
 	"mfussenegger/nvim-lint",
 	event = { "BufReadPre", "BufNewFile" },
--- nvim/lua/mraks/plugins/tmux.lua
+++ nvim/lua/mraks/plugins/tmux.lua
@@ -1,10 +1,10 @@
 return {
-  "christoomey/vim-tmux-navigator",
-  lazy = false,
-  config = function()
-    vim.keymap.set('n','<C-h>', vim.cmd.TmuxNavigateLeft)
-    vim.keymap.set('n','<C-l>', vim.cmd.TmuxNavigateRight)
-    vim.keymap.set('n','<C-j>', vim.cmd.TmuxNavigateDown)
-    vim.keymap.set('n','<C-k>', vim.cmd.TmuxNavigateUp)
-  end
+	"christoomey/vim-tmux-navigator",
+	lazy = false,
+	config = function()
+		vim.keymap.set("n", "<C-h>", vim.cmd.TmuxNavigateLeft)
+		vim.keymap.set("n", "<C-l>", vim.cmd.TmuxNavigateRight)
+		vim.keymap.set("n", "<C-j>", vim.cmd.TmuxNavigateDown)
+		vim.keymap.set("n", "<C-k>", vim.cmd.TmuxNavigateUp)
+	end,
 }
