ccf5040a9a66d78fbdf1d60d43075415d4d9794c
mraksdev <a.krylov.work@gmail.com>
2026-04-20 22:07:58 +0300

    add autocmd to remove trailing ws

--- nvim/lua/mraks/core/autocmds.lua
+++ nvim/lua/mraks/core/autocmds.lua
@@ -10,25 +10,35 @@ local user_group = augroup("MraksConfig", { clear = true })
 
 -- Подсветка скопированного текста (yank)
 autocmd("TextYankPost", {
-  group = user_group,
-  callback = function()
-    vim.highlight.on_yank({ higroup = "IncSearch", timeout = 150 })
-  end,
+	group = user_group,
+	callback = function()
+		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 150 })
+	end,
 })
 
 -- Настройка `colorcolumn` для разных типов файлов
 autocmd("FileType", {
-  group = user_group,
-  pattern = "*",
-  callback = function(args)
-    vim.opt_local.colorcolumn = ""
-    local ft = args.match
-    if ft == "python" then
-      vim.opt_local.colorcolumn = "81"
-    elseif ft == "lua" then
-      vim.opt_local.colorcolumn = "120"
-    elseif ft == "html" or ft == "htmldjango" then
-      vim.opt_local.colorcolumn = "300"
-    end
-  end,
+	group = user_group,
+	pattern = "*",
+	callback = function(args)
+		vim.opt_local.colorcolumn = ""
+		local ft = args.match
+		if ft == "python" then
+			vim.opt_local.colorcolumn = "81"
+		elseif ft == "lua" then
+			vim.opt_local.colorcolumn = "120"
+		elseif ft == "html" or ft == "htmldjango" then
+			vim.opt_local.colorcolumn = "300"
+		end
+	end,
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*",
+	callback = function(args)
+		-- Если файл не оканчивается на ".diff", выполняем команду
+		if not vim.endswith(args.file, ".diff") then
+			vim.cmd([[%s/\s\+$//e]])
+		end
+	end,
 })
